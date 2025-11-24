6909aa8c937bc9062d7daa3073698abaecec82ed
T1mp4ve1 <pavelwire@outlook.it>
2025-11-24 14:48:29 +0100

    delete

---
+++ B_BW1/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace B_BW1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
